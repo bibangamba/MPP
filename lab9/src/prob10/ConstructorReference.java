80fb70a236171106aa8784370d44bd7fd4318cdf
Andrew Twijukye <andrewbibangamba@gmail.com>
2024-04-22 15:47:54 -0500

    9&10

--- lab9/src/prob10/ConstructorReference.java
+++ lab9/src/prob10/ConstructorReference.java
@@ -1,70 +1,84 @@
 package prob10;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
+import java.util.function.BiFunction;
 
-class Human
-{
-	String name;
-	int age;
-	String gender;
-	
-	public Human(String name){
-		this.name = name;
-	}
-	public Human(String name,int age){
-		this.name = name;
-		this.age = age;
-	}
-	
-	public Human(String name,int age, String gender){
-		this.name = name;
-		this.age = age;
-		this.gender = gender;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	@Override
-	public String toString() {
-		return "Human [name=" + name + ", age=" + age + ", gender=" + gender + "]";
-	}
+class Human {
+  String name;
+  int age;
+  String gender;
+
+  public Human(String name) {
+    this.name = name;
+  }
+
+  public Human(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public Human(String name, int age, String gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  @Override
+  public String toString() {
+    return "Human [name=" + name + ", age=" + age + ", gender=" + gender + "]";
+  }
 }
 
 public class ConstructorReference {
-public static void main(String args[]){
-	Human[] list = { new Human("Joe",35,"Male"), new Human("Jane",45,"Female"), new Human("John",30,"Male")};
-	
+  public static void main(String args[]) {
+    Human[] list = {
+      new Human("Joe", 35, "Male"), new Human("Jane", 45, "Female"), new Human("John", 30, "Male")
+    };
+
     // Query 1  : Print only Female canditates names
-	
+    //	Stream.of(list).
+    System.out.println("Female candidate names: ");
+    Arrays.stream(list)
+        .filter(h -> h.gender.equals("Female"))
+        .map(Human::getName)
+        .forEach(System.out::println);
 
-    // Query 2 : Cretae an objecy by choosing suitable Interface to the specified constructors(Totally 3 constuctors)using fouth type of Method Reference ClassName::new. Then print the object status 
- 
-	// Query 3 : Count the male candidates whose age is more than 30
-    
-    
-    
-   }
+    // Query 2 : Cretae an objecy by choosing suitable Interface to the specified
+    // constructors(Totally 3 constuctors)using fouth type of Method Reference ClassName::new. Then
+    // print the object status
+    BiFunction<String, Integer, Human> humanCreator = Human::new;
+    System.out.println("\n<Human::new> BiFunction \n" + humanCreator.apply("Frank", 104));
 
-
-
+    // Query 3 : Count the male candidates whose age is more than 30
+    System.out.println(
+        "\nNumber of male candidates over age 30: "
+            + Arrays.stream(list)
+                .filter(h -> h.gender.equals("Male"))
+                .filter(h -> h.age > 30)
+                .count());
+  }
 }
--- lab9/src/prob9/Dish.java
+++ lab9/src/prob9/Dish.java
@@ -1,51 +1,103 @@
 package prob9;
+
 import java.util.*;
 
 public class Dish {
 
-    private final String name;
-    private final boolean vegetarian;
-    private final int calories;
-    private final Type type;
+  public static final List<Dish> menu =
+      Arrays.asList(
+          new Dish("pork", false, 800, Type.MEAT),
+          new Dish("beef", false, 700, Type.MEAT),
+          new Dish("chicken", false, 400, Type.MEAT),
+          new Dish("french fries", true, 530, Type.OTHER),
+          new Dish("rice", true, 350, Type.OTHER),
+          new Dish("season fruit", true, 120, Type.OTHER),
+          new Dish("pizza", true, 550, Type.OTHER),
+          new Dish("prawns", false, 400, Type.FISH),
+          new Dish("salmon", false, 450, Type.FISH));
+  private final String name;
+  private final boolean vegetarian;
+  private final int calories;
+  private final Type type;
 
-    public Dish(String name, boolean vegetarian, int calories, Type type) {
-        this.name = name;
-        this.vegetarian = vegetarian;
-        this.calories = calories;
-        this.type = type;
-    }
+  public Dish(String name, boolean vegetarian, int calories, Type type) {
+    this.name = name;
+    this.vegetarian = vegetarian;
+    this.calories = calories;
+    this.type = type;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public static void main(String[] args) {
+    System.out.println(
+        "a. Is there any Vegetarian meal available ( return type boolean): " + hasVegan(menu));
+    System.out.println(
+        "b. Is there any healthy menu have calories less than 1000 ( return type boolean): "
+            + hasLowCalories(menu, 1000));
+    System.out.println(
+        "c. Is there any unhealthy menu have calories greater than 1000 ( return type boolean): "
+            + hasHighCalories(menu, 1000));
+    System.out.println(
+        "d. find and return the first item for the type of MEAT( return type Optional<Dish>): "
+            + getFirstMeat(menu).orElse(null));
+    System.out.println(
+        "e. calculateTotalCalories() in the menu using reduce. (return int): "
+            + calculateTotalCalories(menu));
+    System.out.println(
+        "f. calculateTotalCaloriesMethodReference()in the menu using MethodReferences. (return int): "
+            + calculateTotalCaloriesMethodReference(menu));
+  }
 
-    public boolean isVegetarian() {
-        return vegetarian;
-    }
+  public static boolean hasVegan(List<Dish> menu) {
+    return menu.stream().anyMatch(Dish::isVegetarian);
+  }
 
-    public int getCalories() {
-        return calories;
-    }
+  public static boolean hasLowCalories(List<Dish> menu, final int calories) {
+    return menu.stream().anyMatch(d -> d.getCalories() <= calories);
+  }
 
-    public Type getType() {
-        return type;
-    }
+  public static boolean hasHighCalories(List<Dish> menu, final int calories) {
+    return menu.stream().anyMatch(d -> d.getCalories() >= calories);
+  }
 
-    public enum Type { MEAT, FISH, OTHER }
+  public static Optional<Dish> getFirstMeat(List<Dish> menu) {
+    return menu.stream().filter(d -> d.getType() == Type.MEAT).findFirst();
+  }
 
-    @Override
-    public String toString() {
-        return name;
-    }
+  public static Integer calculateTotalCalories(List<Dish> menu) {
+    return menu.stream()
+        .map(Dish::getCalories)
+        .reduce((totalCals, dishCalories) -> totalCals + dishCalories)
+        .orElse(0);
+  }
 
-    public static final List<Dish> menu =
-            Arrays.asList( new Dish("pork", false, 800, Type.MEAT),
-                           new Dish("beef", false, 700, Type.MEAT),
-                           new Dish("chicken", false, 400, Type.MEAT),
-                           new Dish("french fries", true, 530, Type.OTHER),
-                           new Dish("rice", true, 350, Type.OTHER),
-                           new Dish("season fruit", true, 120, Type.OTHER),
-                           new Dish("pizza", true, 550, Type.OTHER),
-                           new Dish("prawns", false, 400, Type.FISH),
-                           new Dish("salmon", false, 450, Type.FISH));
+  public static Integer calculateTotalCaloriesMethodReference(List<Dish> menu) {
+    return menu.stream().map(Dish::getCalories).reduce(Integer::sum).orElse(0);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public boolean isVegetarian() {
+    return vegetarian;
+  }
+
+  public int getCalories() {
+    return calories;
+  }
+
+  public Type getType() {
+    return type;
+  }
+
+  @Override
+  public String toString() {
+    return name;
+  }
+
+  public enum Type {
+    MEAT,
+    FISH,
+    OTHER
+  }
 }
